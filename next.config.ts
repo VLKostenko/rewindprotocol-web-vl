de53dd045ec117f75da29bde8a3d4b4b40631b52
vladimirkostenko <vladimirkostenko4@gmail.com>
2025-07-04 17:13:18 +0300

    test 2

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-// module.exports = {
-//   output: 'export',
-// };
-
 const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_SPACE_ID: process.env.NEXT_PUBLIC_SPACE_ID,
-    NEXT_PUBLIC_ACCESS_TOKEN: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
-  }
+
 };
 
 export default nextConfig;
